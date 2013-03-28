a212b50ed4ac6e11477577e1d9837ec30ac907b9
Robin Abrahamsson <test@test.test>
2013-03-28 22:53:35 +0100

    test

---
+++ GameTest/src/gameTest/GameLoop.java
@@ -0,0 +1,13 @@
+package gameTest;
+
+public class GameLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("play");
+	}
+
+}
